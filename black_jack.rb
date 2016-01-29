ff26ca94cc90b1757834396a30f834e9da152691
ayaka_koike <ayaka.koike@drecom.co.jp>
2016-01-29 20:29:12 +0900

    sue445 feat. hidenba

--- black_jack.rb
+++ black_jack.rb
@@ -3,10 +3,3 @@ Bundler.require
 
 class A
 end
-
-describe A do
-  describe "#new" do
-    expect(1).to eq 1
-  end
-
-end
---
+++ spec/black_jack_spec.rb
@@ -0,0 +1,7 @@
+require_relative "../black_jack"
+
+describe A do
+  describe "#new" do
+    it { expect(1).to eq 1 }
+  end
+end

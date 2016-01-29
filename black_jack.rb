8707c7bb28ddf5da686b58b3f1abf141c4bb1bf4
ayaka_koike <ayaka.koike@drecom.co.jp>
2016-01-29 20:47:41 +0900

    Narazaka feat.tkm_kj

--- black_jack.rb
+++ black_jack.rb
@@ -3,4 +3,12 @@ Bundler.require
 
 class Card
   attr_accessor :number
+  attr_accessor :mark
+  def initialize(number, mark)
+    @number = number
+    @mark = mark
+  end
 end
+
+[:spade, :heart, :clover, :daiya].map do |mark|
+	cards = (1..52).map do Card.new(
